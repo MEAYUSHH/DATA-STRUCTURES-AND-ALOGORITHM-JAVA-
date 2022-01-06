27c5b254f99f38e5b2cd316a3214d66a6366b07f
MEAYUSHH <89283754+MEAYUSHH@users.noreply.github.com>
2022-01-06 20:07:51 +0530

    1st printZ

---
+++ level-1/getting started/basic programs/PrintZ.java
@@ -0,0 +1,10 @@
+// Java first program 
+//learn how to print in java programming langauage
+
+public class printZ {
+
+    public static void main(String[] args) {
+        // Write your code here
+    System.out.print("*****\n   * \n  *  \n *   \n*****");
+    }
+}
