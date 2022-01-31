6d258a177ec05e0332f56f30599af2ec433e5273
AYUSH SHARMA <89283754+MEAYUSHH@users.noreply.github.com>
2022-01-31 14:18:46 +0530

    LinkedList 876

---
+++ leetcode/LinkedList/876.Middle Of LinkedList.java
@@ -0,0 +1,16 @@
+//Two Traversal Approach
+//Hare & Tortoise Approach
+//complexity :O(n)
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head, fast=head;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+        
+    }
+}
